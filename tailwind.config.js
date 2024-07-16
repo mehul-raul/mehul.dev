473fcf0706174217d4637dccf2d4dd82f1f96c81
mehul-raul <161235591+mehul-raul@users.noreply.github.com>
2024-07-16 15:32:03 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // Match all HTML files in the root directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
